92e18e70d53ac5cab4f9f207c3028790428dbeaa
Kai Gittens <kai.gittens@gmail.com>
2017-08-19 13:25:35 -0400

    add code to helpers.js

---
+++ js-build/helpers.js
@@ -0,0 +1,16 @@
+const getPostDiv =  document.querySelectorAll(".post-link-hook");
+
+const doEventOnElement = (element, getEvent, fn) => {
+  for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener(getEvent, event => {
+      fn(element[i])
+    })
+  }
+}
+
+function goToPage(el) {
+  const getArticleLink = el.dataset.url
+  window.location = getArticleLink
+}
+
+export const divClick = doEventOnElement(getPostDiv, 'click', goToPage)
